36aea6efdc2d8ba501515b58f92d340c54e69eb6
Yusufcan Hergül <yusufcan1362@gmail.com>
2023-07-04 23:58:22 +0300

    update

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -1,12 +1,17 @@
 package Pages;
 
 import Utilities.GWD;
+import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.lang.model.element.Element;
+import javax.swing.*;
 import java.time.Duration;
+import java.util.List;
 
 public class Parent {
 
@@ -52,6 +57,41 @@ public class Parent {
                 + "window.scrollBy(0, elementTop-(viewPortHeight/"+doubleCevirilmisHali+"));";
 
         js.executeScript(scrollElementIntoMiddle, element);
+    }
+
+    public void clickFunction(WebElement element){ //düz tıklama
+        scroolToElement(element);
+        waitUntilClickable(element);
+        element.click();
+    }
+
+    public void clickFunction(WebElement element, String Middle){ //ortalayarak tıklama
+        scroolToElement(element, Middle);
+        waitUntilClickable(element);
+        element.click();
+    }
+
+    public void clickFunction(WebElement element, double scrollingPrecision) { //hassas ortalayarak tıklama
+        scroolToElement(element, scrollingPrecision);
+        waitUntilClickable(element);
+        element.click();
+    }
+    public void waitUntilClickable(WebElement element){
+        WebDriverWait wait= new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public void verifyContainsText(WebElement element, String text){
+        scroolToElement(element, "mid");
+        waitUntilVisible(element);
+        Assert.assertTrue(element.getText().toLowerCase().contains(text.toLowerCase()));
+    }
+    public List<WebElement> waitVisibleListAllElement(List<WebElement> elemetList){
+        WebDriverWait wait= new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfAllElements(elemetList));
+    }
+    public void actionFunction(WebElement element){
+        Action actions= new Actions(GWD.getDriver());
+        Action action= actions.moveToElement(element)
     }
 
 
